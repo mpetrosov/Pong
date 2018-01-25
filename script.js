4b0b9e6d4d3768ac9add70c593ef6f49bd3a9285
Riccardo Parrello <riccardoparrello@outlook.com>
2018-01-25 15:26:02 +0100

    Set up javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+/*jslint browser: true*/
+/*jslint node: true */
+/*jslint devel: true */
+
+/*eslint-env browser */
+/*eslint-disable no-unused-vars */
+/*eslint-disable no-console */
+"use strict";
